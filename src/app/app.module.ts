32d1d6752c4a404c01763d7680006ada41cf428b
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-01 16:14:57 +0545

    UI created for project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HomeComponent } from './header/home/home.component';
+import { HeroComponent } from './header/home/hero/hero.component';
+import { SidebarComponent } from './header/home/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    HomeComponent,
+    HeroComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
