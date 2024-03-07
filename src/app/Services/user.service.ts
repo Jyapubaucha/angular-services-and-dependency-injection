953c4d155154096bd448a059f14ee75c1bb6de62
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-07 17:31:26 +0545

    Learned injecting service (logger) into another service (user) with dependency injections.

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,6 +1,9 @@
+import { Injectable } from "@angular/core";
 import { User } from "../Models/User";
+import { LoggerService } from "./logger.service";
 
-export class UserService{
+@Injectable()
+export class UserService {
 
     users: User[] = [
         new User('Deepak Maharjan','Male', 'Monthly', 'Active'),
@@ -8,12 +11,17 @@ export class UserService{
         new User('Dipesh Maharjan','Male','Weekly', 'Active')
     ];
 
+    constructor(private logger: LoggerService){
+
+    }
+
     getAllUsers(){
         return this.users;
     }
-    
+
     createUser(name: string, gender: string, subType: string, status: string){
         let user = new User(name, gender, subType, status);
         this.users.push(user);
+        this.logger.LogMessage(name, status);
     }
 }
